d2f15c331076664ba1a72cce11d5665d966786ff
S-A-S-26 <sujitsas9767190197@gmail.com>
2024-08-25 02:27:30 +0530

    minor ui adjustments

--- vite-project/src/components/Message.tsx
+++ vite-project/src/components/Message.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TopProfile from './TopProfile'
-import { LogOut } from 'lucide-react'
+import { Image, LogOut, Send, Settings, Smile } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Message() {
@@ -17,15 +17,37 @@ export default function Message() {
         <div className='flex justify-between px-7 border h-28 border-l-0 items-center'>
           {/* <h2 className='text-secondary text-3xl font-normal tracking-tighter'></h2> */}
           <TopProfile/>
-          <div className='bg-gray-50 flex p-2 rounded-full transition-all duration-500 group hover:bg-gray-100'>
-            <button className='bg-transparent p-0 border-none' onClick={logout}>
-              <LogOut strokeWidth={1.3}className='text-gray-400 transition-all duration-500 group-hover:text-gray-600'/> 
-            </button>
+          <div className='flex gap-2'>
+            <div className=' flex p-2 rounded-full transition-all duration-500 group hover:bg-gray-100'>
+              <button className='bg-transparent p-0 border-none' onClick={logout}>
+                <LogOut strokeWidth={1.3} size={22}className='text-gray-400 transition-all duration-500 group-hover:text-gray-600'/> 
+              </button>
+            </div>
+            <div className=' flex p-2 rounded-full transition-all duration-500 group hover:bg-gray-100'>
+              <button className='bg-transparent p-0 border-none' onClick={logout}>
+                <Settings strokeWidth={1.3} size={22} className='text-gray-400 transition-all duration-500 group-hover:text-gray-600'/> 
+              </button>
+            </div>
           </div>
-        
         </div>
-        <div className='bg-background grow'>
-
+        <div className='bg-background grow flex flex-col'>
+          <div className='grow'>
+            
+          </div>
+          <div className='h-20 flex items-center justify-center bg-white'>
+            <div className='flex grow gap-3 mx-4 px-8 h-12 items-center rounded-full bg-background'>
+              <button className='bg-transparent p-0 border-none' onClick={logout}>
+                <Smile strokeWidth={1.25} size={22} className='text-gray-400 hover:text-gray-500'/>
+              </button>
+              <input className='grow bg-background outline-none text-gray-500 text-sm' placeholder='Type a message'/>
+              <button className='bg-transparent p-0 border-none' onClick={logout}>
+                <Image strokeWidth={1.25} size={22} className='text-gray-400 hover:text-gray-500'/>
+              </button>
+              <button className='bg-transparent p-0 border-none' onClick={logout}>
+                <Send strokeWidth={1.25} size={22} className='text-gray-400 hover:text-gray-500'/>
+              </button>
+            </div>
+          </div>
         </div>
     </div>
     </>
--- vite-project/src/components/TopProfile.tsx
+++ vite-project/src/components/TopProfile.tsx
@@ -5,13 +5,13 @@ import prof1 from '../assets/dummy1.jpg'
 export default function TopProfile() {
   return (
     <>
-        <div className='flex px-7 py-2 items-center'>
-            <div className='bg-gray-50 w-[75px] h-[75px] rounded-full p-0 m-0 overflow-hidden border-2'>
+        <div className='flex py-2 items-center'>
+            <div className='bg-gray-50 w-[65px] h-[65px] rounded-full p-0 m-0 overflow-hidden'>
                 {/* <CircleUserRound strokeWidth={0.75} size={75} className='h-full w-full text-gray-300'/> */}
                 <img className='object-cover w-full h-full' src={prof1} />
             </div>
             <div className='grow text-left pl-6 grid grid-rows-2'>
-                <div className='tracking-normal text-gray-700 text-2xl tracking-normal'>
+                <div className='tracking-normal text-gray-700 text-xl tracking-normal'>
                     Sujit Sutar
                 </div>
                 <div className='text-gray-500 text-sm'>
